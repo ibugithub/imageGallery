ab07e02e8312b5bb3cd6bd3b54fdf72112e5b909
Md Ibrahim Hossain <outsideworkibrahim@gmail.com>
2023-11-05 02:43:45 +0600

    add header

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
 import './App.css'
 import Gallery from './components/image-gallery/im-gallery'
+import Header from './components/header/header'
 
 const App = () => {
   return (
     <>
+      <Header />
       <Gallery />
     </>
   )
---
+++ src/components/header/header.jsx
@@ -0,0 +1,7 @@
+export const Header = () => {
+  return (
+    <div className='pt-10 text-[#12301a] text-2xl font-serif'>Beautiful Image Gellary</div>
+  )
+}
+
+export default Header;
